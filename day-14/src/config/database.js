4f84694cc4c61aa59f70f7c051c89727d704ba87
Sandeep Vishwakarma <sandeepvishwakarmagkp5@example.com>
2026-02-12 12:36:52 +0530

    initialize a instaClone

---
+++ day-14/src/config/database.js
@@ -0,0 +1,13 @@
+const mongoose  = require('mongoose');
+
+
+async function connectToDB(){
+   await  mongoose.connect(process.env.MONGO_URI).then(()=>{
+        console.log("MongoDB connected Successfully")
+    })
+}
+
+
+module.exports = connectToDB
+
+
