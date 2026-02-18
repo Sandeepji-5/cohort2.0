10a3813e61d94f8671792f2bd2a0b47b120d898a
Sandeep Vishwakarma <sandeepvishwakarmagkp5@example.com>
2026-02-18 18:53:17 +0530

    PostImage, getImage And PostDetails Router  created

---
+++ day-15/src/routes/post.routes.js
@@ -0,0 +1,18 @@
+const express  = require("express")
+const multer = require('multer')
+
+const upload = multer({storage:multer.memoryStorage()})
+const postController = require('../controllers/post.controller');
+const postRouter = express.Router();
+
+postRouter.post("/createPost",upload.single('d'),postController.createPostController)
+
+postRouter.get("/", postController.getPostController)
+postRouter.get("/details/:postId", postController.getPostDetailsController)
+
+/* chacha----  same file name which is sending by frontend.. */
+
+
+module.exports = postRouter;
+
+
